9d0aafc63c21d8f179dacd2faaf1b3f151d65928
Junseok Oh <kotohana5706@edcan.kr>
2016-07-22 09:49:31 +0900

    Implement MainViewActivity

---
+++ app/src/main/java/kr/edcan/kkobugi/MainViewActivity.java
@@ -0,0 +1,13 @@
+package kr.edcan.kkobugi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_view);
+    }
+}
